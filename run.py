c39bf1d933eff72b38960c8e56a2477541dfadf5
Vansh <vanshjjw@gmail.com>
2025-05-21 20:00:05 -0700

    base-4

---
+++ run.py
@@ -0,0 +1,51 @@
+from datasets import load_dataset
+from models import SpeechToTextModel
+from transformers import WhisperProcessor, TrainingArguments, Trainer, AutoTokenizer
+from utils import LibriSpeechDataCollator
+
+print("Loading models...")
+
+whisper_model_name = "openai/whisper-base"
+llama_model_name = "meta-llama/Llama-3.2-3B"
+dataset_name = "openslr/librispeech_asr"
+
+model = SpeechToTextModel(
+    whisper_model_name=whisper_model_name,
+    llama_model_name=llama_model_name,
+    hidden_dims=[2048, 1024, 2048, 1024, 2048],
+    train_whisper=False
+).cuda()
+
+print("Loading dataset...")
+
+dataset = load_dataset(dataset_name, 'clean', split='train.100')
+
+processor = WhisperProcessor.from_pretrained(whisper_model_name)
+tokenizer = AutoTokenizer.from_pretrained(llama_model_name)
+
+print("starts...")
+model.train()
+
+training_args = TrainingArguments(
+    output_dir="./v1-checkpoints",
+    overwrite_output_dir=True,
+    per_device_train_batch_size=1,
+    num_train_epochs=1,
+    logging_steps=10,
+    save_steps=100,
+    bf16=True,
+    remove_unused_columns=False,
+    learning_rate=5e-5,
+    report_to="none",
+    save_safetensors=False,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=LibriSpeechDataCollator(processor, tokenizer),
+)
+
+trainer.train()
+trainer.save_model("./v1-checkpoints")
